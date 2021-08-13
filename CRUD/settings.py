8847ae561b7f4b9e2397a207b6bb0f42197e2b2a
shubhyc7 <86141797+shubhyc7@users.noreply.github.com>
2021-08-13 21:32:57 +0530

    Update settings.py

--- CRUD/settings.py
+++ CRUD/settings.py
@@ -139,7 +139,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # add this line here
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')  # add this line here
 
 STATIC_URL = '/static/'
 
