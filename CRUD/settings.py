055421ace5b7511a6fc8796273e587e6484cde9a
shubhyc7 <86141797+shubhyc7@users.noreply.github.com>
2021-08-14 12:12:12 +0530

    Update settings.py

--- CRUD/settings.py
+++ CRUD/settings.py
@@ -151,7 +151,7 @@ if DEBUG:
 else:
  STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-MEDIA_URL = os.path.join(BASE_DIR, 'media') 
+MEDIA_URL =  '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media') 
 
 # Default primary key field type
